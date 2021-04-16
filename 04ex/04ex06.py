04bf65245eb0d51249c3da108650c489035c5af6
gact17 <76421012+gact17@users.noreply.github.com>
2021-04-16 23:46:49 +0800

    Update 04ex06.py

--- 04ex/04ex06.py
+++ 04ex/04ex06.py
@@ -3,11 +3,11 @@
 def computepay(hours, rate) :
     
     if hours > 40 :
-    reg = rate * hours
-    otp = (hours - 40.0) * (rate * 0.5)
-    pay = reg + otp
+        reg = rate * hours
+        otp = (hours - 40.0) * (rate * 0.5)
+        pay = reg + otp
     else:
-    pay = hours * rate
+        pay = hours * rate
     return pay
 
 sh = input("enter hours: ")
